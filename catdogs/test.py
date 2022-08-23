7089492c4eb04dc62bfded7b0bbe350d90dd96e8
Bobby Lei <bobl53@gmail.com>
2022-08-23 22:42:08 +1200

    Binary classifier for cat or dog images

---
+++ catdogs/test.py
@@ -0,0 +1,23 @@
+import numpy as np
+import os
+from  tensorflow import keras
+from tensorflow.keras.preprocessing import image
+
+model = keras.models.load_model('catordog')
+
+for filename in os.listdir('testimages'):
+  f = os.path.join('testimages', filename)
+
+  # predicting images
+  img = image.load_img(f, target_size=(150, 150))
+  x = image.img_to_array(img) # Converts image to a 2D array
+  x = np.expand_dims(x, axis=0) # Our model is trained on 3D arrays, hence, we convert the 2D to a 3D array
+
+  image_tensor = np.vstack([x])
+  classes = model.predict(image_tensor)
+  print(classes)
+  print(classes[0])
+  if classes[0]>0.5:
+    print(filename + " is a dog")
+  else:
+    print(filename + " is a cat")
---
+++ catdogs/train.py
@@ -0,0 +1,75 @@
+from pickletools import optimize
+from tensorflow.keras import layers
+from tensorflow.keras import Model
+from tensorflow.keras.applications.inception_v3 import InceptionV3
+import urllib.request
+from tkinter import Image
+import tensorflow as tf
+from keras.preprocessing.image import ImageDataGenerator
+from keras.optimizers import RMSprop
+
+TRAINING_DIR = "/Users/BobbyLei/Desktop/learn/aimlcoder/catdogs/cat-or-dog/training/"
+# Experiment with your own parameters here to really try to drive it to 99.9% accuracy or better
+train_datagen = ImageDataGenerator(rescale=1./255,
+    #   rotation_range=40,
+    #   width_shift_range=0.2,
+    #   height_shift_range=0.2,
+    #   shear_range=0.2,
+    #   zoom_range=0.2,
+    #   horizontal_flip=True,
+    #   fill_mode='nearest'
+      )
+train_generator = train_datagen.flow_from_directory(TRAINING_DIR,
+                                                    batch_size=100,
+                                                    class_mode='binary',
+                                                    target_size=(150, 150))
+
+VALIDATION_DIR = "/Users/BobbyLei/Desktop/learn/aimlcoder/catdogs/cat-or-dog/testing/"
+# Experiment with your own parameters here to really try to drive it to 99.9% accuracy or better
+validation_datagen = ImageDataGenerator(rescale=1./255)
+validation_generator = validation_datagen.flow_from_directory(VALIDATION_DIR,
+                                                              batch_size=100,
+                                                              class_mode='binary',
+                                                              target_size=(150, 150))
+
+weights_url = "https://storage.googleapis.com/mledu-datasets/inception_v3_weights_tf_dim_ordering_tf_kernels_notop.h5"
+weights_file = "inception_v3.h5"
+urllib.request.urlretrieve(weights_url, weights_file)
+
+pre_trained_model = InceptionV3(input_shape=(150, 150, 3),
+                                include_top=False,
+                                weights=None)
+
+pre_trained_model.load_weights(weights_file)
+
+for layer in pre_trained_model.layers:
+    layer.trainable = False
+
+# pre_trained_model.summary()
+
+last_layer = pre_trained_model.get_layer('mixed7')
+print('last layer output shape: ', last_layer.output_shape)
+last_output = last_layer.output
+
+# Flatten the output layer to 1 dimension
+x = layers.Flatten()(last_output)
+# Add a fully connected layer with 1,024 hidden units and ReLU activation
+x = layers.Dense(1024, activation='relu')(x)
+# Add a dropout rate of 0.2
+#x = layers.Dropout(0.2)(x)
+# Add a final sigmoid layer for classification
+x = layers.Dense(1, activation='sigmoid')(x)
+
+model = Model(pre_trained_model.input, x)
+
+model.compile(optimizer=RMSprop(learning_rate=0.007),
+              loss='binary_crossentropy',
+              metrics=['accuracy'])
+
+history = model.fit(
+            train_generator,
+            validation_data=validation_generator,
+            epochs=20,
+            verbose=1)
+
+model.save('catordog')
